a416065ca564b3b4d8d0032176412b8e597d558d
ambiesoft <ambiesoft.trueff@gmail.com>
2022-01-27 23:19:12 +0900

    learn python and solidity programming in vscode

--- web3pytest/web3_py_simple_storage/deploy.py
+++ web3pytest/web3_py_simple_storage/deploy.py
@@ -1,11 +1,16 @@
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
+from dotenv import load_dotenv
+import os
 
-# install_solc("0.6.0")
+load_dotenv()
+
+install_solc("0.6.0")
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    # print(simple_storage_file)
 
 
 # Compile Our Solidity
@@ -37,4 +42,57 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+chain_id = 1337
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# private_key = "0xaae19050a820ed79da88bb3fabac6ddee5c0f4d1c539550b2c072872a1baca5f"
+private_key = os.getenv("PRIVATE_KEY")
+# print(private_key)
 
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+# transaction = SimpleStorage.constructor().buildTransaction(
+#     {"chainId": chain_id, "from": my_address, "nonce": nonce})
+transaction = SimpleStorage.constructor().buildTransaction({
+    "gasPrice": w3.eth.gas_price,
+    # "gas": 700000,
+    # "gasPrice": 100,
+    "chainId": chain_id,
+    "from": my_address,
+    "nonce": nonce,
+})
+# print(transaction)
+
+signed_txn = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key)
+# print(signed_txn)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+
+# working with the contract
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# <Function retrieve() bound to ()>
+# print(simple_storage.functions.retrieve())
+print(simple_storage.functions.retrieve().call())
+
+# Call -> Simulate making the call and getting a return value
+# Transact -> Actually make a state change
+# print(simple_storage.functions.store(15).call())
+store_transaction = simple_storage.functions.store(15).buildTransaction({
+    "gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": my_address, "nonce": nonce+1
+})
+signed_store_tx = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print(simple_storage.functions.retrieve().call())
--- web3pytest/web3_py_simple_storage/startvscode.bat
+++ web3pytest/web3_py_simple_storage/startvscode.bat
@@ -1,5 +1,7 @@
 set PATH=%PATH:C:\Linkout\bat;=%
 call C:\Linkout\bat\envpython3.bat
+call C:\Linkout\bat\envnode.bat
+call C:\Linkout\bat\envyarn.bat
 
 start "" C:\local\VSCode\Code.exe "%~dp0"
 
